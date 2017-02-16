49129093ed0ddaec191c69676afabab92c9f1aa1
Jan Taylor <janttaylor@gmail.com>
2017-02-16 14:18:45 -0700

    Added logo, adjusted CSS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $('.button-collapse').sideNav();
+  $('.button-collapse').sideNav({
+    menuWidth: 100, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  });
   $('.parallax').parallax();
   $('.scrollspy').scrollSpy();
 });
